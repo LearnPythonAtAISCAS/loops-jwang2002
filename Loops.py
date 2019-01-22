9dbf1a00d5a83c553eff3cdeb534ed6ea110daa7
Adj <32112632+Adren-aline@users.noreply.github.com>
2019-01-22 13:05:44 -0500

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -8,13 +8,16 @@ def loops():
     ## For loop
     # The format of a for loop is as follows 
     # for boolean statement
-    # the boolean statement can be itterating through a number or array
+    # the boolean statement can be iterating through a number or array
     # example
     for i in range(15): #goes upto 14
         print (i) #Outputs numbers from 1 to 14
 
     # Now try iterating so that the variable c is multiplied by itself 15 times
     c = 2
+    for x in range(16):
+        c = c ** 2
+        print(c)
     
     # a while statement may be used to wait for a certain thing to be true
     a = 1 
